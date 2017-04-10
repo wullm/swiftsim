393039c120842cc9295e6774cfb285ec94eb8c4e
Stefan Arridge <stefan.arridge@durham.ac.uk>
2017-04-10 18:44:13 +0100

    Self gravity test simply has random unit mass particles, added error message for when cells are empty

--- examples/self_gravity_test/makeIC_DM.py
+++ examples/self_gravity_test/makeIC_DM.py
@@ -48,16 +48,23 @@ boxSize = 2.
 
 
 # First particle is at the centre
+# Second particle is at [1,0,0]
 # initally put the particles at apoapsis on the x-axis, semi-major axis is 1.0
-coords = np.zeros((n+1,3))
-coords[1:,:] = np.random.random((n,3))
-# centre particle is stationary, put other particles on random orbits
-vels = np.zeros((n+1,3))
-vels[1:,:] = np.random.random((n,3))
+#coords = np.zeros((n,3))
+#coords[1,:] = [1,0,0]
+coords = np.random.random((n,3))
 
-# centre particle has mass 1, other particles have 0 mass
-mass = np.zeros(n+1)
-mass[0] = 1.0
+# centre particle is stationary, second particle has velocity 1 in the y-direction, other particles have random velocities
+#vels = np.zeros((n,3))
+#vels[1,:] = [0,1,0]
+vels = np.random.random((n,3))
+
+# 1st and second particles have mass 1, other particles have 0 mass
+#mass = np.zeros(n)
+#mass[0] = 1.0
+#mass[1] = 1.0
+mass = np.full((n,),1.0)
+
 #r = np.zeros(n)
 #vels = np.zeros(n+1,3)
 #v_mag = np.zeros(n+1
@@ -86,14 +93,15 @@ grp.attrs["Unit temperature in cgs (U_T)"] = 1.
 grp = file.create_group("/RuntimePars")
 grp.attrs["PeriodicBoundariesOn"] = periodic
 
-coords += np.full((n+1,3),boxSize/2.)
+# shift particles to centre of box
+coords += np.full((n,3),boxSize/2.)
 
 # Header
 grp = file.create_group("/Header")
 grp.attrs["BoxSize"] = boxSize
-grp.attrs["NumPart_Total"] =  [0 , n+1, 0, 0, 0, 0]
+grp.attrs["NumPart_Total"] =  [0 , n, 0, 0, 0, 0]
 grp.attrs["NumPart_Total_HighWord"] = [0, 0, 0, 0, 0, 0]
-grp.attrs["NumPart_ThisFile"] = [0, n+1, 0, 0, 0, 0]
+grp.attrs["NumPart_ThisFile"] = [0, n, 0, 0, 0, 0]
 grp.attrs["Time"] = 0.0
 grp.attrs["NumFilesPerSnapshot"] = 1
 grp.attrs["MassTable"] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
@@ -103,22 +111,22 @@ grp.attrs["Dimension"] = 3
 # Particle group
 grp = file.create_group("/PartType1")
 
-ds = grp.create_dataset('Coordinates', (n+1, 3), 'd')
+ds = grp.create_dataset('Coordinates', (n, 3), 'd')
 ds[()] = coords
 coords = np.zeros(1)
 
-ds = grp.create_dataset('Velocities', (n+1, 3), 'f')
+ds = grp.create_dataset('Velocities', (n, 3), 'f')
 ds[()] = vels
 vels = np.zeros(1)
 
-ds = grp.create_dataset('Masses', (n+1, ), 'f')
+ds = grp.create_dataset('Masses', (n, ), 'f')
 ds[()] = mass
 mass = np.zeros(1)
 
 
 # Particle IDs
-ids = 1 + np.linspace(0, n+1, n+1, endpoint=False)
-ds = grp.create_dataset('ParticleIDs', (n+1, ), 'L')
+ids = 1 + np.linspace(0, n, n, endpoint=False)
+ds = grp.create_dataset('ParticleIDs', (n, ), 'L')
 ds[()] = ids
 
 file.close()
