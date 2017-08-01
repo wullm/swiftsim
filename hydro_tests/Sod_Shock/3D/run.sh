6cbb00aa21a0e0cb260c520b8721b085c52b2093
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2017-08-01 15:24:54 +0100

    Reduced the number of tests in the 3D Sod shock convergence test.

--- hydro_tests/Sod_Shock/3D/run.sh
+++ hydro_tests/Sod_Shock/3D/run.sh
@@ -13,7 +13,7 @@ do
   source hydro_tests/Sod_Shock/3D/"$scheme"_config.sh
   make -j 16
   cd hydro_tests/Sod_Shock/3D/
-  for n in 1 2 3 4 5
+  for n in 1 2 3
   do
     folder="$scheme"_"$n"
     mkdir $folder
