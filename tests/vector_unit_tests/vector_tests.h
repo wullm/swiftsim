715546ee74a104dd7a47cb1e154aba89441e87e8
James Willis <james.s.willis@durham.ac.uk>
2019-02-12 14:29:41 +0000

    Include minmax.h in the vector_unit_tests header file.

--- tests/vector_unit_tests/vector_tests.h
+++ tests/vector_unit_tests/vector_tests.h
@@ -1,6 +1,7 @@
 #ifndef SWIFT_VECTOR_TESTS
 #define SWIFT_VECTOR_TESTS
 #include "../../config.h"
+#include "../../src/minmax.h"
 #include "../../src/vector.h"
 #include <stdio.h>
 #include <math.h>
