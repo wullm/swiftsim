12babebaecfbc93eb0f38a15734fb14439a2c447
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-08-13 17:53:47 +0200

    initial commit ivanova dirty

---
+++ src/todo_temporary_globals.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "error.h"
+#include "todo_temporary_globals.h"
+
+
+
+struct mladen_globals mladen_globs;
+
+/* sets up the necessary stuff. To be called in main.
+ * Just drop in everything you need to be done first
+ * in here.
+ */
+void mladen_setup(void){
+
+  
+
+  mladen_globs.outfilep = fopen("mladen_outputfile_all.txt", "w");  
+  mladen_globs.testing = 23;
+  printf(" size of float      : %lu\n", sizeof(float));
+  printf(" size of float    : %lu\n", sizeof(float));
+  printf(" size of double     : %lu\n", sizeof(double));
+
+  message("MESSAGE TO MLADEN: calling init your own global temporary stuff.");
+}
+
+
+
+void mladen_cleanup(void){
+  
+  fclose(mladen_globs.outfilep);
+}
+
+
+
