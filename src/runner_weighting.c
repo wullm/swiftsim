2a5d1f8cdb0af32453301d03e4e351d583b8cf9f
Willem Elbers <willem.elbers@gmail.com>
2020-01-22 14:16:26 +0000

    Momentum weighting

--- src/runner_weighting.c
+++ src/runner_weighting.c
@@ -87,12 +87,13 @@ void runner_do_weighting(struct runner *r, struct cell *c, int timer) {
           if (e->step == 0) {
             gp->f_phase_i = fermi_dirac_density(e, gp->x, gp->v_full);
             gp->f_phase = gp->f_phase_i;
-            // gp->g_phase_i = sample_density(e, gp->x, gp->v_full);
+            gp->g_phase_i = fermi_dirac_momentum(e, gp->v_full);
             gp->mass = 1e-10; //dither in the first time step
           } else {
             gp->f_phase = fermi_dirac_density(e, gp->x, gp->v_full);
-            gp->mass = particle_mass * (gp->f_phase_i - gp->f_phase) / gp->f_phase_i;
-            gp->f_phase_i = 0.99 * gp->f_phase_i + 0.01 * gp->f_phase;
+            double pnow = fermi_dirac_momentum(e, gp->v_full);
+            gp->mass = particle_mass * (1.0 - gp->f_phase / gp->f_phase_i * gp->g_phase_i / pnow);
+            // gp->f_phase_i = 0.99 * gp->f_phase_i + 0.01 * gp->f_phase;
 
             // gp->mass = particle_mass * (gp->f_phase - gp->f_phase_i);
 
@@ -101,7 +102,7 @@ void runner_do_weighting(struct runner *r, struct cell *c, int timer) {
             //     double vy = gp->v_full[1];
             //     double vz = gp->v_full[2];
             //     // double v = sqrt(vx*vx+vy*vy+vz*vz);
-            //     message("%i \t%.10e \t%f \t%f \t%f \t%f \t%.10e", (int) gp->id_or_neg_offset, gp->mass, vx, vy, vz, gp->f_phase_i, gp->f_phase);
+            //     message("%i \t%.10e \t%f \t%f \t%f \t%f \t%f", (int) gp->id_or_neg_offset, gp->mass, vx, vy, vz, gp->f_phase_i, gp->f_phase);
             // }
 
           }
