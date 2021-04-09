fa9ed97152d298b1590a0774247f524ab44fc1c5
Willem Elbers <willem.h.elbers@durham.ac.uk>
2021-04-09 17:36:53 +0100

    Centre of absolute mass

--- src/multipole_struct.h
+++ src/multipole_struct.h
@@ -133,8 +133,9 @@ struct multipole {
 #if SELF_GRAVITY_MULTIPOLE_ORDER > 0
 
   /* 1st order terms (all 0 since we expand around CoM) */
-  // float M_100, M_010, M_001;
-
+#if 1
+  float M_100, M_010, M_001;
+#endif
 #endif
 #if SELF_GRAVITY_MULTIPOLE_ORDER > 1
 
