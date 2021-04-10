1020ab0233beec4e5a1fd979f0324957a0a60b53
Willem Elbers <willem.h.elbers@durham.ac.uk>
2021-04-10 18:30:54 +0100

    Set multipole CoM to something sensible in degenerate cases

--- src/multipole.h
+++ src/multipole.h
@@ -970,110 +970,6 @@ __attribute__((nonnull)) INLINE static void gravity_multipole_compute_power(
 }
 
 /**
- * @brief Zero out a multipole.
- *
- * @param multi The #multipole (content will  be overwritten).
- */
-__attribute__((nonnull)) INLINE static void gravity_clear_multipole(
-    struct gravity_tensors *multi) {
-  multi->r_max = 0.0;
-  multi->CoM[0] = 0.0;
-  multi->CoM[1] = 0.0;
-  multi->CoM[2] = 0.0;
-  multi->m_pole.max_softening = 0.0;
-  multi->m_pole.min_old_a_grav_norm = 0.0;
-  multi->m_pole.vel[0] = 0.0;
-  multi->m_pole.vel[1] = 0.0;
-  multi->m_pole.vel[2] = 0.0;
-  multi->m_pole.max_delta_vel[0] = 0.0;
-  multi->m_pole.max_delta_vel[1] = 0.0;
-  multi->m_pole.max_delta_vel[2] = 0.0;
-  multi->m_pole.min_delta_vel[0] = 0.0;
-  multi->m_pole.min_delta_vel[1] = 0.0;
-  multi->m_pole.min_delta_vel[2] = 0.0;
-  multi->m_pole.M_000 = 0.0;
-
-#if SELF_GRAVITY_MULTIPOLE_ORDER > 0
-
-  /* 1st order terms (all 0 since we expand around CoM) */
-  // multi->m_pole.M_100 = 0.0;
-  // multi->m_pole.M_010 = 0.0;
-  // multi->m_pole.M_001 = 0.0;
-#endif
-#if SELF_GRAVITY_MULTIPOLE_ORDER > 1
-
-  /* 2nd order terms */
-  multi->m_pole.M_200 = 0.0;
-  multi->m_pole.M_020 = 0.0;
-  multi->m_pole.M_002 = 0.0;
-  multi->m_pole.M_110 = 0.0;
-  multi->m_pole.M_101 = 0.0;
-  multi->m_pole.M_011 = 0.0;
-#endif
-#if SELF_GRAVITY_MULTIPOLE_ORDER > 2
-
-  /* 3rd order terms */
-  multi->m_pole.M_300 = 0.0;
-  multi->m_pole.M_030 = 0.0;
-  multi->m_pole.M_003 = 0.0;
-  multi->m_pole.M_210 = 0.0;
-  multi->m_pole.M_201 = 0.0;
-  multi->m_pole.M_120 = 0.0;
-  multi->m_pole.M_021 = 0.0;
-  multi->m_pole.M_102 = 0.0;
-  multi->m_pole.M_012 = 0.0;
-  multi->m_pole.M_111 = 0.0;
-#endif
-#if SELF_GRAVITY_MULTIPOLE_ORDER > 3
-
-  /* 4th order terms */
-  multi->m_pole.M_400 = 0.0;
-  multi->m_pole.M_040 = 0.0;
-  multi->m_pole.M_004 = 0.0;
-  multi->m_pole.M_310 = 0.0;
-  multi->m_pole.M_301 = 0.0;
-  multi->m_pole.M_130 = 0.0;
-  multi->m_pole.M_031 = 0.0;
-  multi->m_pole.M_103 = 0.0;
-  multi->m_pole.M_013 = 0.0;
-  multi->m_pole.M_220 = 0.0;
-  multi->m_pole.M_202 = 0.0;
-  multi->m_pole.M_022 = 0.0;
-  multi->m_pole.M_211 = 0.0;
-  multi->m_pole.M_121 = 0.0;
-  multi->m_pole.M_112 = 0.0;
-#endif
-#if SELF_GRAVITY_MULTIPOLE_ORDER > 4
-
-  /* 5th order terms */
-  multi->m_pole.M_005 = 0.0;
-  multi->m_pole.M_014 = 0.0;
-  multi->m_pole.M_023 = 0.0;
-  multi->m_pole.M_032 = 0.0;
-  multi->m_pole.M_041 = 0.0;
-  multi->m_pole.M_050 = 0.0;
-  multi->m_pole.M_104 = 0.0;
-  multi->m_pole.M_113 = 0.0;
-  multi->m_pole.M_122 = 0.0;
-  multi->m_pole.M_131 = 0.0;
-  multi->m_pole.M_140 = 0.0;
-  multi->m_pole.M_203 = 0.0;
-  multi->m_pole.M_212 = 0.0;
-  multi->m_pole.M_221 = 0.0;
-  multi->m_pole.M_230 = 0.0;
-  multi->m_pole.M_302 = 0.0;
-  multi->m_pole.M_311 = 0.0;
-  multi->m_pole.M_320 = 0.0;
-  multi->m_pole.M_401 = 0.0;
-  multi->m_pole.M_410 = 0.0;
-  multi->m_pole.M_500 = 0.0;
-#endif
-#if SELF_GRAVITY_MULTIPOLE_ORDER > 5
-#error "Missing implementation for order >5"
-#endif
-}
-
-/**
  * @brief Constructs the #multipole of a bunch of particles around their
  * centre of mass.
  *
@@ -1119,9 +1015,17 @@ __attribute__((nonnull)) INLINE static void gravity_P2M(
     vel[2] += gparts[k].v_full[2] * m;
   }
 
-  /* Terminate early if the positive and negative masses almost cancel */
-  if (abs_mass > 1.01 * fabsf(mass)) {
-    gravity_clear_multipole(multi);
+  /* Handle cases where positive and negative masses almost cancel */
+  const double alpha = 1.0 - 0.01;
+  if (fabsf(mass) < alpha * abs_mass) {
+    /* Set the values to something sensible */
+    gravity_multipole_init(&c->grav.multipole->m_pole);
+    c->grav.multipole->CoM[0] = gparts[0]->x[0];
+    c->grav.multipole->CoM[1] = gparts[0]->x[1];
+    c->grav.multipole->CoM[2] = gparts[0]->x[2];
+    c->grav.multipole->r_max = 0.;
+
+    /* And terminate early */
     return;
   }
 
